a1e1af8d284076417f723d47a556e014b65d4e56
caseLearner0718 <105021025@live.asia.edu.tw>
2016-09-19 16:46:40 +0800

    hw

--- src/hw/hw01_105021025.java
+++ src/hw/hw01_105021025.java
@@ -6,9 +6,9 @@ public class hw01_1050210XX {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
 		System.out.print("請輸入n:");
-		double n = scn.nextInt();
-		n=Math.pow(n,1/3.0);
-		System.out.print((int)n);
+		double a = scn.nextDouble();
+		double b = Math.ceil(Math.log(a)/Math.log(2));
+		System.out.print("共需" + b + "位元")
 	}
 
 }
